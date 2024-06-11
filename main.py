56edd41c697f8c95159ddfdebafc18660bc3742f
Marta Makowska <martakmakowska@gmail.com>
2024-06-11 18:22:35 +0200

    Trying to make xml signature

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+import base64
+import hashlib
+import os
+
 from Szyfrowanie.Szyfrowanie import Szyfrowanie
 from cryptography.fernet import Fernet
+
+from Szyfrowanie.Xades import Xades
+from GUI import Application
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-#def szyfrowanie():
-#    key = Fernet.generate_key()
-#    with open("mykey.key", "wb") as f:
-#        f.write(key)
+def encoding(pin: str):
+    key = get_key_from_pin(pin)
+    with open("mykey.key", "wb") as f:
+        f.write(key)
+
+def get_key_from_pin(pin: str):
+    key = hashlib.sha256(pin.encode()).digest()
+    key = base64.urlsafe_b64encode(key)
+    return key
 
 def getting_key():
     with open("mykey.key", "rb") as f:
@@ -33,11 +45,20 @@ def decrypting_file(key: Fernet):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # szyfrowanie_publiczne = Szyfrowanie()
+    szyfrowanie = Szyfrowanie()
     print_hi('PyCharm')
+    file_name = "main.cpp"
+    # szyfrowanie.sing_file(file_name)
+    #szyfrowanie.verify_file(file_name)
     #key = getting_key()
     # encrypting_file(Fernet(key))
     #decrypting_file(Fernet(key))
+    encoding("1234")
+    xades = Xades()
+    # xades.sign(file_name)
+    # xades.create_xades_signature(file_name=file_name)
+    app = Application()
+
 
 
 
