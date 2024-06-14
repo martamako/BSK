03590e319e931877f9c86d631c1c783bb6611a76
Marta Makowska <martakmakowska@gmail.com>
2024-06-14 11:14:00 +0200

    Adding function to create hash and encrypt/decrypt file

--- Szyfrowanie/Szyfrowanie.py
+++ Szyfrowanie/Szyfrowanie.py
@@ -1,38 +1,32 @@
 import os.path
-
 import rsa
-from Crypto.Random import get_random_bytes
+import hashlib
+import base64
+from cryptography.fernet import Fernet
+
+from Szyfrowanie.Xades import Xades
+
 
 class Szyfrowanie:
     def __init__(self):
-        if not (os.path.exists("public.pem") and os.path.exists("private.pem")):
-            self.creating_keys()
+        self.__pin = '12345'
+        self.aes_key = self.get_key_from_pin(self.__pin)
 
         with open("public.pem", "rb") as f:
             self.__public_key = rsa.PublicKey.load_pkcs1(f.read())
 
-        with open("private.pem", "rb") as f:
-            self.__private_key = rsa.PrivateKey.load_pkcs1(f.read())
+        self.xades = Xades()
 
-    def creating_keys(self, key_length: int = 4096):
-        public_key, private_key = rsa.newkeys(key_length)
-        self.write_key_to_file(public_key, "public.pem")
-        self.write_key_to_file(private_key, "private.pem")
+    def get_key_from_pin(self, pin: str):
+        key = hashlib.sha256(pin.encode()).digest()
+        key = base64.urlsafe_b64encode(key)
+        with open("mykey.key", "wb") as f:
+            f.write(key)
+        return key
 
-    def write_key_to_file(self, key, file_name: str):
-        with open(file_name, "wb") as f:
-            f.write(key.save_pkcs1("PEM"))
+    def check_pin(self, pin: str):
+        return self.get_key_from_pin(pin) == self.get_key_from_pin(self.__pin)
 
-    def encode(self, message: str, key):
-        encrypted_message = rsa.encrypt(message.encode(), key)
-
-        with open("encrypted.message", "wb") as f:
-            f.write(encrypted_message)
-
-    def decode(self, key):
-        encrypted_message = open("encrypted.message", "rb").read()
-        clear_message = rsa.decrypt(encrypted_message, key)
-        print(clear_message.decode())
 
     def sign(self, message: str):
         signature = rsa.sign(message.encode(), self.__private_key, "SHA-256")
@@ -40,7 +34,60 @@ class Szyfrowanie:
         with open("signature", "wb") as f:
             f.write(signature)
 
-    def verify(self, message: str):
-        signature = open("signature", "rb").read()
-        verification = rsa.verify(message.encode(), signature, self.__public_key)
+    def sing_file(self, file_name: str, key_name):
+
+        private_key = self.decrypting_key(key_name)
+        hash_of_file = self.document_hash(file_name)
+
+        private_key = rsa.PrivateKey.load_pkcs1(private_key)
+        signature = rsa.sign(hash_of_file, private_key, "SHA-256")
+        signature_base64 = base64.b64encode(signature).decode('utf-8')
+
+        self.xades.sign(file_name, signature_base64, hash_of_file)
+
+        with open("file_signature", "wb") as file_signature:
+            file_signature.write(signature)
+
+    def document_hash(self, file_name: str):
+        file_content = open(file_name, "rb").read()
+
+        file_hash = hashlib.sha256(file_content).digest()
+        file_hash = base64.urlsafe_b64encode(file_hash)
+        return file_hash
+    def verify_file(self, file_name: str):
+        file_content = open(file_name, "rb").read()
+        signature = open("file_signature", "rb").read()
+        verification = rsa.verify(file_content, signature, self.__public_key)
         print(verification)
+
+    def encrypt_file(self, file_name: str):
+        file = open(file_name, "rb").read()
+        encrypted = rsa.encrypt(file, self.__public_key)
+        with open(file_name, "rb") as f:
+            f.write(encrypted)
+
+
+    def decrypt_file(self, file_name: str):
+        file = open(file_name, "rb").read()
+        decrypted = rsa.decrypt(file, self.__public_key)
+        with open(file_name, "rb") as f:
+            f.write(decrypted)
+
+
+    def encrypting_key(self):
+        with open("private.pem", "rb") as private_pem:
+            private_key = private_pem.read()
+        key = Fernet(self.aes_key)
+        encrypted = key.encrypt(private_key)
+
+        with open("private.pem", "wb") as encrypted_file:
+            encrypted_file.write(encrypted)
+
+    def decrypting_key(self, key_name: str):
+        with open("private.pem", "rb") as encrypted_file:
+            encrypted = encrypted_file.read()
+        key = Fernet(self.aes_key)
+        decrypted = key.decrypt(encrypted)
+        # with open("private.pem", "wb") as decrypted_file:
+        #    decrypted_file.write(decrypted)
+        return decrypted
