a2d74c0efcb6a12ba2e9eb9878e5e1390b689084
Marta Makowska <martakmakowska@gmail.com>
2024-06-14 22:09:31 +0200

    Removing class Keys and refactoring functions to include necessary arguments.

--- Szyfrowanie/Keys.py
+++ Szyfrowanie/Keys.py
@@ -1,23 +1,48 @@
+import base64
+import hashlib
 import os
 import rsa
+from cryptography.fernet import Fernet
 
 
-class Keys:
-    def __init__(self):
-        if not (os.path.exists("public.pem") and os.path.exists("private.pem")):
-            self.creating_keys()
+def creating_keys(key_length: int = 4096):
+    public_key, private_key = rsa.newkeys(key_length)
+    write_key_to_file(public_key, "public.pem")
+    write_key_to_file(private_key, "private.pem")
 
-        with open("public.pem", "rb") as f:
-            self.__public_key = rsa.PublicKey.load_pkcs1(f.read())
 
-        with open("private.pem", "rb") as f:
-            self.__private_key = rsa.PrivateKey.load_pkcs1(f.read())
+def get_key_from_pin(pin: str):
+    key = hashlib.sha256(pin.encode()).digest()
+    key = base64.urlsafe_b64encode(key)
+    #with open("mykey.key", "wb") as f:
+    #    f.write(key)
+    return key
 
-    def creating_keys(self, key_length: int = 4096):
-        public_key, private_key = rsa.newkeys(key_length)
-        self.write_key_to_file(public_key, "public.pem")
-        self.write_key_to_file(private_key, "private.pem")
 
-    def write_key_to_file(self, key, file_name: str):
-        with open(file_name, "wb") as f:
-            f.write(key.save_pkcs1("PEM"))
+def write_key_to_file(key, file_name: str):
+    with open(file_name, "wb") as f:
+        f.write(key.save_pkcs1("PEM"))
+
+
+def encrypting_key(aes_key: bytes):
+    with open("private.pem", "rb") as private_pem:
+        private_key = private_pem.read()
+    key = Fernet(aes_key)
+    encrypted = key.encrypt(private_key)
+
+    with open("private.pem", "wb") as encrypted_file:
+        encrypted_file.write(encrypted)
+
+
+def decrypting_key(key_path: str, aes_key: bytes):
+    with open(key_path, "rb") as encrypted_file:
+        encrypted = encrypted_file.read()
+    key = Fernet(aes_key)
+    return key.decrypt(encrypted)
+    # with open("private.pem", "wb") as decrypted_file:
+    #    decrypted_file.write(decrypted)
+
+
+if __name__ == "__main__":
+    if not (os.path.exists("public.pem") and os.path.exists("private.pem")):
+        creating_keys()
