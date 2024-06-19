20898fe18497fb9a67e2435901db7239142c9898
Marta Makowska <martakmakowska@gmail.com>
2024-06-19 15:41:02 +0200

    Added USBMonitor where app knows if pendrive is connected or not

--- GUI.py
+++ GUI.py
@@ -3,19 +3,24 @@ This module provides functionality:
 - Creating GUI app emulate electronic signature
 """
 from tkinter import *
+from tkinter import messagebox
 from tkinter.filedialog import askopenfilename
 from Encrypting.Encrypting import *
+from usbmonitor import USBMonitor
+from usbmonitor.attributes import ID_MODEL, ID_MODEL_ID, ID_VENDOR_ID
 
 
 class Page:
     """
     Class Page is base class for creating pages with different functionalities.
     """
+
     def __init__(self, main_frame: Frame):
         """
         Constructor taking main frame of application's window.
         :param main_frame: Frame created from application's window. It's frame on which all other frames will be placed.
         """
+        self.label = None
         self.key_path = None
         self.file_path = None
         self.main_frame = main_frame
@@ -26,17 +31,22 @@ class Page:
         icon = PhotoImage(file='icons/default.png')
         self.window.iconphoto(False, icon)
 
-    def page(self, text: str, document_page_str: str, key_page_str: str):
+    def page(self, text: str, document_page_str: str, key_page_str: str, connected_usb: bool):
         """
         Creating page with text for button, document_page_str for label in document frame
         and key_page_str for label in key frame.
         :param text: Text that will be on button.
         :param document_page_str: Text displayed on label before button to choose file
         :param key_page_str: Text displayed on label before button to choose file with key
+        :param connected_usb: Information if pendrive is connected
         :return:
         """
         self.delete_pages()
         frame = Frame(self.main_frame)
+
+        self.label = Label(frame, text="USB device: ")
+        self.set_usb_label(connected_usb)
+        self.label.pack(pady=10)
 
         button = Button(frame, text=text, font=("Bold", 12), fg="#158aff", bd=0,
                         bg="#c3c3c3", command=self.functionality)
@@ -126,11 +136,23 @@ class Page:
             print(f"Wybrany klucz: {file_path}")
             label.config(text=f"Wybrany klucz: {file_path}")
 
+    def set_usb_label(self, connected: bool):
+        """
+        Setting label with information if pendrive is connected
+        :param connected: parameter if USB device is connected.
+        :return:
+        """
+        if connected:
+            self.label.config(text=f"USB device connected")
+        else:
+            self.label.config(text="USB device disconnected")
+
 
 class SigningPage(Page):
     """
     Subclass of class Page. This class is meant to create page for signing functionality.
     """
+
     def __init__(self, main_frame: Frame):
         super().__init__(main_frame)
         self.icon = PhotoImage(file='icons/signing.png')
@@ -138,17 +160,23 @@ class SigningPage(Page):
         self.entry = None
         self.page()
 
-    def page(self, text="Podpisanie", document_page_str="Plik do podpisania", key_page_str="Zaszyfrowany klucz prywatny"):
+    def page(self, text="Podpisanie", document_page_str="Plik do podpisania",
+             key_page_str="Zaszyfrowany klucz prywatny", connected_usb: bool = False):
         """
         Implementation of method page. Parameters are set to text for signing page functionality.
         To parent class it also adds enetering PIN to decrypt private key.
         :param text: Text that will be on button.
         :param document_page_str: Text displayed on label before button to choose file
         :param key_page_str: Text displayed on label before button to choose file with key
+        :param connected_usb: Information if pendrive is connected
         :return:
         """
         self.delete_pages()
         frame = Frame(self.main_frame)
+
+        self.label = Label(frame, text="USB device: ")
+        self.set_usb_label(connected_usb)
+        self.label.pack(pady=10)
 
         self.document_page(document_page_str)
         self.key_page(key_page_str)
@@ -193,14 +221,16 @@ class ValidationPage(Page):
     """
     Subclass of class Page. This class is meant to create page for validation functionality.
     """
+
     def __init__(self, main_frame: Frame):
         super().__init__(main_frame)
         self.icon = PhotoImage(file='icons/validation.png')
         self.window = main_frame.winfo_toplevel()
         # self.page()
 
-    def page(self, text="Walidacja", document_page_str="Plik do weryfikacji", key_page_str="Klucz publiczny"):
-        super().page(text, document_page_str, key_page_str)
+    def page(self, text="Walidacja", document_page_str="Plik do weryfikacji",
+             key_page_str="Klucz publiczny",  connected_usb: bool = False):
+        super().page(text, document_page_str, key_page_str, connected_usb)
 
     def functionality(self):
         """
@@ -216,13 +246,15 @@ class EncryptingPage(Page):
     """
     Subclass of class Page. This class is meant to create page for encrypting functionality.
     """
+
     def __init__(self, main_frame: Frame):
         super().__init__(main_frame)
         self.icon = PhotoImage(file='icons/encryption.png')
         self.window = main_frame.winfo_toplevel()
 
-    def page(self, text="Szyfrowanie", document_page_str="Plik do zaszyfrowania", key_page_str="Klucz publiczny"):
-        super().page(text, document_page_str, key_page_str)
+    def page(self, text="Szyfrowanie", document_page_str="Plik do zaszyfrowania",
+             key_page_str="Klucz publiczny",  connected_usb: bool = False):
+        super().page(text, document_page_str, key_page_str, connected_usb)
 
     def functionality(self):
         """
@@ -238,13 +270,15 @@ class DecryptingPage(Page):
     """
     Subclass of class Page. This class is meant to create page for decrypting functionality.
     """
+
     def __init__(self, main_frame: Frame):
         super().__init__(main_frame)
         self.icon = PhotoImage(file='icons/decryption.png')
         self.window = main_frame.winfo_toplevel()
 
-    def page(self, text="Odszyfrowanie", document_page_str="Plik do odszyfrowania", key_page_str="Klucz prywatny"):
-        super().page(text, document_page_str, key_page_str)
+    def page(self, text="Odszyfrowanie", document_page_str="Plik do odszyfrowania",
+             key_page_str="Klucz prywatny", connected_usb: bool = False):
+        super().page(text, document_page_str, key_page_str, connected_usb)
 
     def functionality(self):
         """
@@ -260,10 +294,23 @@ class App:
     """
     Class responsible for creating GUI app
     """
+
     def __init__(self):
         """
         Creates window and all necessary elements to display app
         """
+        self.connected_usb = False
+        self.device_info_str = lambda device_info: \
+            f"{device_info[ID_MODEL]} ({device_info[ID_MODEL_ID]} - {device_info[ID_VENDOR_ID]})"
+
+        # Start the USB monitoring thread
+        self.monitor = USBMonitor()
+
+        # Start the daemon
+        self.monitor.start_monitoring(on_connect=self.on_connect, on_disconnect=self.on_disconnect)
+        if self.find_pendrive():
+            self.connected_usb = True
+
         self.window = Tk()
         self.main_frame = Frame(self.window, highlightbackground='black', highlightthickness=2)
         self.options_frame = Frame(self.window, bg="#c3c3c3")
@@ -280,7 +327,35 @@ class App:
         self.decrypting_page = DecryptingPage(self.main_frame)
         self.decrypting_btn = None
 
+        self.current_page = self.signing_page
+
         self.create_window()
+        self.current_page.set_usb_label(self.connected_usb)
+        self.window.mainloop()
+
+    def find_pendrive(self) -> bool:
+        connected_devices = self.monitor.get_available_devices()
+        for device in connected_devices:
+            usb_class = connected_devices[device]['ID_USB_CLASS_FROM_DATABASE']
+            if usb_class == 'DiskDrive':
+                return True
+        return False
+
+    def on_connect(self, device_id, device_info):
+        # self.show_message("USB Device Added", f"Device added: {device_info}")
+        print(f"Connected: {self.device_info_str(device_info=device_info)}")
+        self.connected_usb = True
+        self.current_page.set_usb_label(self.connected_usb)
+
+    def on_disconnect(self, device_id, device_info):
+        # self.show_message("USB Device Removed", f"Device removed: {device_info}")
+        print(f"Disconnected: {self.device_info_str(device_info=device_info)}")
+        self.connected_usb = False
+        self.current_page.set_usb_label(self.connected_usb)
+
+    def show_message(self, title, message):
+        # Schedule the message box display on the main thread
+        self.window.after(0, lambda: messagebox.showinfo(title, message))
 
     def create_window(self):
         """
@@ -292,8 +367,6 @@ class App:
 
         self.create_menu()
         self.create_main_frame()
-
-        self.window.mainloop()
 
     def create_main_frame(self):
         """
@@ -314,20 +387,28 @@ class App:
         self.options_frame.configure(width=200, height=500)
 
         self.signing_btn = Button(self.options_frame, text="Podpisywanie", font=("Bold", 15), fg="#158aff", bd=0,
-                                  bg="#c3c3c3", command=self.signing_page.page)
+                                  bg="#c3c3c3",
+                                  command=lambda: self.button_click(self.signing_page))
         self.signing_btn.place(x=10, y=100)
 
         self.validation_btn = Button(self.options_frame, text="Walidacja", font=("Bold", 15), fg="#158aff", bd=0,
-                                     bg="#c3c3c3", command=self.validation_page.page)
+                                     bg="#c3c3c3",
+                                     command=lambda: self.button_click(self.validation_page))
         self.validation_btn.place(x=10, y=150)
 
         self.encrypting_btn = Button(self.options_frame, text="Szyfrowanie", font=("Bold", 15), fg="#158aff", bd=0,
-                                     bg="#c3c3c3", command=self.encrypting_page.page)
+                                     bg="#c3c3c3",
+                                     command=lambda: self.button_click(self.encrypting_page))
         self.encrypting_btn.place(x=10, y=200)
 
         self.decrypting_btn = Button(self.options_frame, text="Deszyfrowanie", font=("Bold", 15), fg="#158aff", bd=0,
-                                     bg="#c3c3c3", command=self.decrypting_page.page)
+                                     bg="#c3c3c3",
+                                     command=lambda: self.button_click(self.decrypting_page))
         self.decrypting_btn.place(x=10, y=250)
+
+    def button_click(self, page: Page):
+        self.current_page = page
+        self.current_page.page(connected_usb=self.connected_usb)
 
 
 if __name__ == "__main__":
